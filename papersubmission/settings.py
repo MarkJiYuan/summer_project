2596dda6bd35aa1a7627f56133466994ab0259b7
郑吉源 <zhengjiyuan@zhengjiyuandeMacBook-Pro.local>
2019-12-16 22:02:32 +0800

    use mysql

--- papersubmission/settings.py
+++ papersubmission/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'papersubmission.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'paper',
+        'USER': 'root',
+        'PASSWORD': '521Luozhenyi',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
